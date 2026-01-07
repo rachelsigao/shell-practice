4ecfaef61ced13b42e8beea3f4578e25772cc011
rachelsigao <rachelsigao@gmail.com>
2026-01-07 13:53:06 -0500

    hello world started

---
+++ 03-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+PERSON1=Rachel
+PERSON2=Rocky
+
+echo "$PERSON1: Hey $PERSON2, How are you?"
+echo "$PERSON2: Hey $PERSON1, Am doing good. How about you?"
+echo "$PERSON1: Doing great, what's up?"
+echo "$PERSON2: Nothing much, browsing some business ideas"
+echo "$PERSON1: Okies cool..."
