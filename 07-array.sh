abccbf9305cf941a4fa3cf33997a34590b80cc31
rachelsigao <rachelsigao@gmail.com>
2026-01-07 16:09:31 -0500

    hello world started

--- 07-array.sh
+++ 07-array.sh
@@ -2,4 +2,7 @@
 
 Movies=("Zero" "One" "Two" "Three" "Four")
 
-echo "First Movie: $(Movies[0])"
+echo "First Movie: ${Movies[0]}"
+echo "Third Movie: ${Movies[2]}"
+
+echo "All Movie: ${Movies[@]}"
