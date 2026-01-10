e7ef2d248886342909aa754364094048aa6d81b3
rachelsigao <rachelsigao@gmail.com>
2026-01-10 15:59:14 -0500

    hello world started

---
+++ 10-install-command.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+if ($USERID -NE 0)
+then
+    echo "ERROR: Please run this script with root access"
+else
+    echo "You are running with root access, Script is being installed"
+fi
+
+dnf install mysql -y
