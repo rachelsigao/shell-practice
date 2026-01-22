a4bea3e360ec12d76070e404b62b797f3b05f12e
rachelsigao <rachelsigao@gmail.com>
2026-01-22 16:07:37 -0500

    message

---
+++ 18-delete-oldlogs.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+LOGS_FOLDER="/var/log/shellscript-logs" 
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+SOURCE_DIR=/home/ec2-user/app-logs
+
+mkdir -p $LOGS_FOLDER
+
+#check for root access
+if [ $USERID -ne 0 ]
+then
+    echo -e "$R ERROR: $N Please run this script with root access" | tee -a $LOG_FILE
+    exit 1 #give other than 0 upto 127
+else
+    echo "You are running with root access, Script is being installed" | tee -a $LOG_FILE
+fi
+
+#Validate function takes input as exit status, what command they tried to install
+VALIDATE ()
+    {
+    if [ $1 -eq 0 ]
+    then 
+        echo -e "$2 is $G SUCCESS $N" | tee -a $LOG_FILE
+    else 
+        echo -e "$2 is $R FAILURE $N" | tee -a $LOG_FILE
+        exit 1
+    fi
+    }
+
+
+echo "Script started executing at: $(date)" | tee -a $LOG_FILE
+
+FILES_TO_DELETE=$(find . -name "*.log" -mtime +14)
+
+while IFS= read -r filepath
+do 
+    echo "Deleting old log files: $filepath" | tee -a $LOG_FILE
+    rm -rf $filepath
+    <<< $FILES_TO_DELETE
+done
+
+echo "Script executed successfully" | tee -a $LOG_FILE
