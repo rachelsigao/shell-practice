42674d5837ecb935f84ea052a8b8d84acb0e69ae
rachelsigao <rachelsigao@gmail.com>
2026-01-22 19:15:00 -0500

    message

--- 20-backup.sh
+++ 20-backup.sh
@@ -9,19 +9,31 @@ LOGS_FOLDER="/var/log/shellscript-logs"
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
 
-mkdir -p $LOGS_FOLDER
+#Validate function takes input as exit status, what command they tried to install
+VALIDATE ()
+    {
+    if [ $1 -eq 0 ]
+    then 
+        echo -e "$2 is $G SUCCESS $N" | tee -a $LOG_FILE
+    else 
+        echo -e "$2 is $R FAILURE $N" | tee -a $LOG_FILE
+        exit 1
+    fi
+    }
+
 
 #check for root access
 check_root() {
-if [ $USERID -ne 0 ]
-then
-    echo -e "$R ERROR: $N Please run this script with root access" | tee -a $LOG_FILE
-    exit 1 #give other than 0 upto 127
-else
-    echo "You are running with root access, Script is being installed" | tee -a $LOG_FILE
-fi
+    if [ $USERID -ne 0 ]
+    then
+        echo -e "$R ERROR: $N Please run this script with root access" | tee -a $LOG_FILE
+        exit 1 #give other than 0 upto 127
+    else
+        echo "You are running with root access, Script is being installed" | tee -a $LOG_FILE
+    fi
 } 
 
+mkdir -p $LOGS_FOLDER
 
 USAGE() {
     echo -e "$R USAGE: $N sh 20-backup.sh <source-dir> <destination-dir> <days(optional)>"
@@ -34,15 +46,5 @@ fi
 
 
 
-#Validate function takes input as exit status, what command they tried to install
-VALIDATE ()
-    {
-    if [ $1 -eq 0 ]
-    then 
-        echo -e "$2 is $G SUCCESS $N" | tee -a $LOG_FILE
-    else 
-        echo -e "$2 is $R FAILURE $N" | tee -a $LOG_FILE
-        exit 1
-    fi
-    }
+
 
