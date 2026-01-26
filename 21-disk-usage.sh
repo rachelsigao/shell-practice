f7a54445ad0960a5345c89eed3a1896290cfba6d
rachelsigao <rachelsigao@gmail.com>
2026-01-26 17:47:10 -0500

    message

--- 21-disk-usage.sh
+++ 21-disk-usage.sh
@@ -11,7 +11,7 @@ do
     PARTITION=$(echo $line | awk '{print $7F}')
     if [ $USAGE -ge $DISK_THRESHOLD ]
     then
-        MSG+="High Disk Usage on ${PARTITION}: $USAGE%<br>" #<br> represents HTML new line
+        MSG+="High Disk Usage on $PARTITION: $USAGE%<br>" #<br> represents HTML new line
         echo "$MSG"
     fi
 done <<< $DISK_USAGE
