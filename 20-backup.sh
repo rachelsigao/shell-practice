6709af2dd8f209606809e4fb5df946d512716ff1
rachelsigao <rachelsigao@gmail.com>
2026-01-26 15:52:01 -0500

    message

--- 20-backup.sh
+++ 20-backup.sh
@@ -80,14 +80,19 @@ then
     ZIP_NAME="app-logs-$TIMESTAMP.zip" 
     
     cd "$SOURCE_DIR" || exit 1 #change to source directory, -m = move files into zip (delete originals automatically)
-    echo "$FILES" | sed "s|$SOURCE_DIR/||" | zip -m -@ "$ZIP_NAME" #zipping the files and removing source directory path
+    echo "$FILES" | sed "s|$SOURCE_DIR/||" | zip -@ "$ZIP_NAME" #zipping the files and removing source directory path
     
     #check if zip file is created
     if [ -f $ZIP_NAME ] 
     then
         echo -e "$G Successfully created Zip file $N" | tee -a $LOG_FILE
+        while IFS= read -r filepath #delete the files in source directory after zip (zip only creates copy of the files not the files themselves.)
+        do
+            rm -f "$filepath"
+            echo "Deleting file: $filepath" | tee -a $LOG_FILE
+        done <<< "$FILES"
         
-        mv "$ZIP_NAME" "$DEST_DIR" #move zip file to destination directory
+        mv "$ZIP_NAME" "$DEST_DIR/" #move zip file to destination directory
         echo -e "Moving zip file to destination directory is... $G SUCCESS $N"
 
     else
